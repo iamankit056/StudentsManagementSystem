cea0ea008337e170b265d64a95c4cb1844ec60ac
Ankit <iamankit056@gmail.com>
2022-10-27 00:53:33 +0530

    Day 3

--- StudentsManagementSystem/Forms/IndexForm.Designer.cs
+++ StudentsManagementSystem/Forms/IndexForm.Designer.cs
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StudentsDGrid = new System.Windows.Forms.DataGridView();
+            this.IndexTitleLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NewRegistrationBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.mySqlConnection1 = new MySql.Data.MySqlClient.MySqlConnection();
+            this.RollNumberCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DepartmentCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EmailCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PasswordCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsDGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StudentsDGrid
+            // 
+            this.StudentsDGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StudentsDGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentsDGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RollNumberCol,
+            this.NameCol,
+            this.DepartmentCol,
+            this.EmailCol,
+            this.PasswordCol});
+            this.StudentsDGrid.Location = new System.Drawing.Point(12, 57);
+            this.StudentsDGrid.Name = "StudentsDGrid";
+            this.StudentsDGrid.Size = new System.Drawing.Size(776, 345);
+            this.StudentsDGrid.TabIndex = 1;
+            // 
+            // IndexTitleLabel
+            // 
+            this.IndexTitleLabel.AutoSize = true;
+            this.IndexTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IndexTitleLabel.Location = new System.Drawing.Point(12, 21);
+            this.IndexTitleLabel.Name = "IndexTitleLabel";
+            this.IndexTitleLabel.Size = new System.Drawing.Size(196, 33);
+            this.IndexTitleLabel.TabIndex = 3;
+            this.IndexTitleLabel.Text = "Student Panel";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(504, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(438, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Search";
+            // 
+            // NewRegistrationBtn
+            // 
+            this.NewRegistrationBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.NewRegistrationBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewRegistrationBtn.Location = new System.Drawing.Point(250, 408);
+            this.NewRegistrationBtn.Name = "NewRegistrationBtn";
+            this.NewRegistrationBtn.Size = new System.Drawing.Size(130, 35);
+            this.NewRegistrationBtn.TabIndex = 6;
+            this.NewRegistrationBtn.Text = "New Registration";
+            this.NewRegistrationBtn.UseVisualStyleBackColor = true;
+            // 
+            // EditBtn
+            // 
+            this.EditBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.EditBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditBtn.Location = new System.Drawing.Point(386, 408);
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.Size = new System.Drawing.Size(130, 35);
+            this.EditBtn.TabIndex = 7;
+            this.EditBtn.Text = "Edit";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.DeleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteBtn.Location = new System.Drawing.Point(522, 408);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(130, 35);
+            this.DeleteBtn.TabIndex = 8;
+            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(658, 408);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 35);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Log Out";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // RollNumberCol
+            // 
+            this.RollNumberCol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.RollNumberCol.HeaderText = "Roll Number";
+            this.RollNumberCol.Name = "RollNumberCol";
+            // 
+            // NameCol
+            // 
+            this.NameCol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NameCol.HeaderText = "Name";
+            this.NameCol.Name = "NameCol";
+            // 
+            // DepartmentCol
+            // 
+            this.DepartmentCol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DepartmentCol.HeaderText = "Department";
+            this.DepartmentCol.Name = "DepartmentCol";
+            // 
+            // EmailCol
+            // 
+            this.EmailCol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.EmailCol.HeaderText = "Email";
+            this.EmailCol.Name = "EmailCol";
+            // 
+            // PasswordCol
+            // 
+            this.PasswordCol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.PasswordCol.HeaderText = "Password";
+            this.PasswordCol.Name = "PasswordCol";
+            // 
+            // IndexForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.EditBtn);
+            this.Controls.Add(this.NewRegistrationBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.IndexTitleLabel);
+            this.Controls.Add(this.StudentsDGrid);
+            this.Name = "IndexForm";
             this.Text = "IndexForm";
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsDGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView StudentsDGrid;
+        private System.Windows.Forms.Label IndexTitleLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button NewRegistrationBtn;
+        private System.Windows.Forms.Button EditBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button button3;
+        private MySql.Data.MySqlClient.MySqlConnection mySqlConnection1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RollNumberCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DepartmentCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmailCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PasswordCol;
     }
 }
